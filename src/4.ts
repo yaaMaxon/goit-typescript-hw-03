314d4fd33394bd4abbf26a1684010eba7810d89c
Maksym <maks.sansei@gmail.com>
2023-11-19 23:14:04 +0200

    hw 3

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,60 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+   private key: Key;
+
+   constructor(key: Key) {
+    this.key = key;
+   }
+
+   getKey(): Key {
+    return this.key;
+   }
+}
+
+abstract class House {
+   protected door: boolean = false;
+   protected key: Key;
+   protected tenants: Person[] = [];
+
+   constructor(key: Key) {
+    this.key = key;
+   }
+
+   abstract openDoor(key: Key): void;
+
+   comeIn(person: Person): void {
+    if (this.door) {
+        this.tenants.push(person);
+        console.log('Welcome home!')
+    } else {
+        console.log('The door is closed...')
+    }
+   }
+
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log('The door is open')
+        } else {
+            console.log('The door is closed...')
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
